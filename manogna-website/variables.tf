4b841f6e0ca58c35fa105653b5ed621a2d1f8e8e
Manogna <72064499+manogna-chinta@users.noreply.github.com>
2023-09-03 13:09:17 +0530

    Update variables.tf

--- manogna-website/variables.tf
+++ manogna-website/variables.tf
@@ -9,3 +9,5 @@ variable "private_data_subnet_az1_cidr" {}
 variable "private_data_subnet_az2_cidr" {}
 variable database_username {}
 variable database_password {}
+variable certificate_domain_name {}
+variable additional_domain_name {}
