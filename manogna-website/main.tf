4827534a3ef1c1c218f50510619fd368b9de2cbe
Manogna <72064499+manogna-chinta@users.noreply.github.com>
2023-09-03 13:05:47 +0530

    Update main.tf

--- manogna-website/main.tf
+++ manogna-website/main.tf
@@ -45,3 +45,44 @@ module "rds" {
   database_username    = var.database_username
   database_password    = var.database_password
 }
+# creating Key for instances
+module "key" {
+  source = "../modules/key"
+}
+# Creating Application Load balancer
+module "alb" {
+  source         = "../modules/alb"
+  project_name   = module.vpc.project_name
+  alb_security_group_id      = module.security-group.alb_security_group_id
+  public_subnet_az1_id = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id = module.vpc.public_subnet_az2_id
+  vpc_id         = module.vpc.vpc_id
+}
+
+module "asg" {
+  source                     = "../modules/asg"
+  project_name               = module.vpc.project_name
+  key_name                   = module.key.key_name
+  client_security_group_id   = module.security-group.client_security_group_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  tg_arn         = module.alb.tg_arn
+
+}
+
+# create cloudfront distribution 
+module "cloudfront" {
+  source = "../modules/cloudfront"
+  certificate_domain_name = var.certificate_domain_name
+  alb_domain_name = module.alb.alb_dns_name
+  additional_domain_name = var.additional_domain_name
+  project_name = module.vpc.project_name
+}
+# Add record in route 53 hosted zone
+
+module "route53" {
+  source = "../modules/route53"
+  cloudfront_domain_name = module.cloudfront.cloudfront_domain_name
+  cloudfront_hosted_zone_id = module.cloudfront.cloudfront_hosted_zone_id
+
+}
