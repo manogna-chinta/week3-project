b0e92edad37689447b3b7e85bc9b6ff0756ffe23
Manogna <72064499+manogna-chinta@users.noreply.github.com>
2023-09-03 12:59:08 +0530

    Update main.tf

--- modules/nat-gateway/main.tf
+++ modules/nat-gateway/main.tf
@@ -90,7 +90,7 @@ resource "aws_route_table_association" "private_app_subnet_az2_route_table_az2_a
 }
 
 # associate private data subnet az2 with private route table az2
-resource "aws_route_table_association" "private_data_subnet_az2_route_table_az2_association" {
-  subnet_id         = var.private_data_subnet_az2_id
-  route_table_id    = aws_route_table.private_route_table_az2.id
-}
+ resource "aws_route_table_association" "private_data_subnet_az2_route_table_az2_association" {
+   subnet_id         = var.private_data_subnet_az2_id
+   route_table_id    = aws_route_table.private_route_table_az2.id
+ }
